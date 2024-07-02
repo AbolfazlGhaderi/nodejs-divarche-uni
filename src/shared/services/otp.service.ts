1cf77965f35067b2820137a892e0f4cd4bf31929
AbolfazlGhaderi <brianprogrammer0@gmail.com>
2024-07-02 10:42:35 +0330

    Feat(OTPService): OtpService has been added -> node-cach

---
+++ src/shared/services/otp.service.ts
@@ -0,0 +1,27 @@
+import NodeCache from 'node-cache';
+import crypto from 'crypto';
+
+export abstract class OtpService {
+  private CACHE: NodeCache = new NodeCache();
+
+  // Save Otp Code
+  /**
+   * ttl: Seconds
+   * @param key
+   * @param value
+   * @param ttl
+   */
+  async SetOtpLogin(key: string, value: string, ttl: number) {
+    this.CACHE.set(key, value, ttl);
+  }
+
+  // Get Otp Code
+  async GetOtpLogin(key: string) {
+    return this.CACHE.get(key);
+  }
+
+  // Generate Otp Code
+  public GenerateOtpCode(length: number): string {
+    return crypto.randomInt(10 ** (length - 1), 10 ** length - 1).toString();
+  }
+}
