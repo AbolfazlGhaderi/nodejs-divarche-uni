0b22ffc82666c0a0973b860ab6018729d8855eb8
AbolfazlGhaderi <brianprogrammer0@gmail.com>
2024-07-02 17:24:44 +0330

    Refactor(AuthService): AuthService updated .

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import { LoginDto } from './dto/login.dto';
 import { inject, injectable } from 'inversify';
 import { IOCTYPES } from '../../iOC/ioc.types';
 import { UserService } from '../user/user.service';
 import { UserRepository } from '../user/user.repository';
 import { SessionRepository } from './session.repository';
+import { LoginCheckOtpDto, LoginDto } from './dto/login.dto';
 import { OtpService } from '../../shared/services/otp.service';
 import { IAuthService } from './interface/auth.service.interface';
 import { TokenService } from '../../shared/services/token.service';
@@ -42,10 +42,19 @@ export class AuthService implements IAuthService {
       });
     }
 
-    return this.SaveAndSendOtpLogin(phoneNumber, key, req, res);
+    const { code, otpToken } = await this.LoginSaveAndSendOtp(phoneNumber, key, req, res);
+
+    // Set Cookie
+    res.cookie(CookieNameEnum.OtpToken, otpToken, {
+      maxAge: 1000 * 60 * 2, // 2 minutes
+      httpOnly: true,
+      secure: IN_PRODUCTION,
+      sameSite: 'lax',
+    });
+    return res.render('./check-otp', { pageTitle: 'otp - DivarChe', otpCode: code, otpError: undefined });
   }
 
-  async SaveAndSendOtpLogin(phoneNumber: string, key: string, req: express.Request, res: express.Response) {
+  async LoginSaveAndSendOtp(phoneNumber: string, key: string, req: express.Request, res: express.Response) {
     // Generate Otp
     const code = this.otpService.GenerateOtpCode(5);
 
@@ -55,16 +64,10 @@ export class AuthService implements IAuthService {
     await this.otpService.SetOtp(key, code, 120);
 
     // Generate Otp Token
-    const OtpToken = this.tokenService.sign({ phoneNumber }, '2m');
+    const otpToken = this.tokenService.sign({ phoneNumber }, '2m');
 
-    // Set Cookie
-    res.cookie(CookieNameEnum.OtpToken, OtpToken, {
-      maxAge: 1000 * 60 * 2, // 2 minutes
-      httpOnly: true,
-      secure: IN_PRODUCTION,
-      sameSite: 'lax',
-    });
-
-    return res.render('./check-otp', { pageTitle: 'otp - DivarChe', otpCode: code, otpError: undefined });
+    return { code, otpToken };
   }
+
+
 }
